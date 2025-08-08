00c9d9938e51900d075bd3ddc27c7c639a800306
Kevin Smith <vndinmchine@gmail.com>
2025-08-07 20:52:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -105,6 +105,3 @@ async def login_post(request: Request, password: str = Form(...)):
         "error": "Invalid password",
         "now": datetime.now
     })
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
